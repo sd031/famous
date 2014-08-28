e3365b9d33c0f4f8af421150d07ef60c5adb14b7
Andrew Reedy <andrew.reedy@thecontrolgroup.com>
2014-08-28 15:35:45 -0400

    update

--- inputs/FastClick.js
+++ inputs/FastClick.js
@@ -1,4 +1,3 @@
-Famono.scope('famous/inputs/FastClick', [], function(require, define) {
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -89,6 +88,4 @@ define(function(require, exports, module) {
         }
         return false;
     }
-});
-
 });
